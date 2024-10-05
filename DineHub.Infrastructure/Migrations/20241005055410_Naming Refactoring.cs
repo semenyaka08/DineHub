b9626a6a9a84c72689ddf7816f7d566cf7033081
Semenyaka Sergey <Serh.semka@gmail.com>
2024-10-05 13:55:55 +0300

    Added migration

---
+++ DineHub.Infrastructure/Migrations/20241005055410_Naming Refactoring.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DineHub.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class NamingRefactoring : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Dish_Restaurant_RestaurantId",
+                table: "Dish");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Restaurant",
+                table: "Restaurant");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Dish",
+                table: "Dish");
+
+            migrationBuilder.RenameTable(
+                name: "Restaurant",
+                newName: "Restaurants");
+
+            migrationBuilder.RenameTable(
+                name: "Dish",
+                newName: "Dishes");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Dish_RestaurantId",
+                table: "Dishes",
+                newName: "IX_Dishes_RestaurantId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Restaurants",
+                table: "Restaurants",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Dishes",
+                table: "Dishes",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Dishes_Restaurants_RestaurantId",
+                table: "Dishes",
+                column: "RestaurantId",
+                principalTable: "Restaurants",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Dishes_Restaurants_RestaurantId",
+                table: "Dishes");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Restaurants",
+                table: "Restaurants");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Dishes",
+                table: "Dishes");
+
+            migrationBuilder.RenameTable(
+                name: "Restaurants",
+                newName: "Restaurant");
+
+            migrationBuilder.RenameTable(
+                name: "Dishes",
+                newName: "Dish");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Dishes_RestaurantId",
+                table: "Dish",
+                newName: "IX_Dish_RestaurantId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Restaurant",
+                table: "Restaurant",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Dish",
+                table: "Dish",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Dish_Restaurant_RestaurantId",
+                table: "Dish",
+                column: "RestaurantId",
+                principalTable: "Restaurant",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}

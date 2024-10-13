dba723803bab4a705e607330f9750df82469cd85
Semenyaka Sergey <Serh.semka@gmail.com>
2024-10-13 19:36:44 +0300

    Adjust project structure

---
+++ src/DineHub.Infrastructure/Migrations/20241009171615_AddRatingFieldToRestaurantEntity.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DineHub.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRatingFieldToRestaurantEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "Rating",
+                table: "Restaurants",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Rating",
+                table: "Restaurants");
+        }
+    }
+}
